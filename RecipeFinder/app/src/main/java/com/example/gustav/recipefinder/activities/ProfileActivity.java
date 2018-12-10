401f7b5970e9513fc9a07b45149eac8eee598da9
GustavP Svensson <gustavps97@gmail.com>
2018-12-10 15:42:18 +0100

    Friendsystem färdigt, kvar att göra: Visa en lista över ens vänner och friendrequest på sin egen profil för sig själv.

--- RecipeFinder/app/src/main/java/com/example/gustav/recipefinder/activities/ProfileActivity.java
+++ RecipeFinder/app/src/main/java/com/example/gustav/recipefinder/activities/ProfileActivity.java
@@ -10,6 +10,9 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.gustav.recipefinder.R;
 import com.example.gustav.recipefinder.adapters.ProfileAdapter;
@@ -20,16 +23,23 @@ import com.example.gustav.recipefinder.fragments.ProfileTop;
 import com.example.gustav.recipefinder.fragments.Settings;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class ProfileActivity extends AppCompatActivity implements ProfileAdmin.OnFragmentInteractionListener , Settings.OnFragmentInteractionListener, BookmarkList.OnFragmentInteractionListener{
+public class ProfileActivity extends AppCompatActivity implements ProfileTop.OnFragmentInteractionListener, ProfileAdmin.OnFragmentInteractionListener , Settings.OnFragmentInteractionListener, BookmarkList.OnFragmentInteractionListener{
 
     private FirebaseAuth mAuth;
     private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabase;
+    private Fragment bookmarkFrag;
     private String uID;
+
+    private TextView friends_warning_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +49,8 @@ public class ProfileActivity extends AppCompatActivity implements ProfileAdmin.O
 
         mAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        friends_warning_text = findViewById(R.id.not_friends_warning);
 
 
         FragmentManager manager = getSupportFragmentManager();
@@ -49,18 +61,18 @@ public class ProfileActivity extends AppCompatActivity implements ProfileAdmin.O
         transaction.add(R.id.fragment_top, fragment);
         transaction.commit();
 
+
         // Bottom fragment
         if(mFirebaseUser != null) {
             Fragment frag;
             if(mFirebaseUser.getUid().equals(uID)) {
-                frag = new ProfileAdmin();
-            } else {
-                frag = BookmarkList.newInstance(uID);
-            }
-            transaction = manager.beginTransaction();
-            transaction.replace(R.id.fragment_container, frag);
-            transaction.commit();
+                friends_warning_text.setVisibility(View.GONE);
 
+                frag = new ProfileAdmin();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_container, frag);
+                transaction.commit();
+            }
         }
     }
 
@@ -92,5 +104,62 @@ public class ProfileActivity extends AppCompatActivity implements ProfileAdmin.O
         transaction.replace(R.id.fragment_container, frag);
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    @Override
+    public void remove_friend(String friend_id) {
+        mDatabase.child("user_friends/"+mAuth.getUid()+"/"+friend_id).removeValue();
+        mDatabase.child("user_friends/"+friend_id+"/"+mAuth.getUid()).removeValue();
+    }
+
+    @Override
+    public void add_friend(String friend_id) {
+        DatabaseReference ref = mDatabase.child("friend_requests/"+friend_id);
+        Map<String, Object> updates = new HashMap<>();
+        updates.put(mAuth.getUid(), true);
+        ref.updateChildren(updates);
+    }
+
+    @Override
+    public void cancel_friend_request(String friend_id) {
+        mDatabase.child("friend_requests/"+friend_id+"/"+mAuth.getUid()).removeValue();
+    }
+
+    @Override
+    public void accept_friend_request(String friend_id) {
+        mDatabase.child("friend_requests/"+mAuth.getUid()+"/"+friend_id).removeValue();
+
+        DatabaseReference ref = mDatabase.child("user_friends/"+mAuth.getUid());
+        Map<String, Object> updates = new HashMap<>();
+        updates.put(friend_id, true);
+        ref.updateChildren(updates);
+
+        ref = mDatabase.child("user_friends/"+friend_id);
+        updates = new HashMap<>();
+        updates.put(mAuth.getUid(), true);
+        ref.updateChildren(updates);
+    }
+
+    @Override
+    public void deny_friend_request(String friend_id) {
+        mDatabase.child("friend_requests/"+mAuth.getUid()+"/"+friend_id).removeValue();
+    }
+
+    @Override
+    public void friend_status_changed(boolean friends) {
+        if(friends) {
+            friends_warning_text.setVisibility(View.GONE);
+
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            bookmarkFrag = BookmarkList.newInstance(uID);
+            transaction.replace(R.id.fragment_container, bookmarkFrag, "bookmark_frag");
+            transaction.addToBackStack(null);
+            transaction.commit();
+        } else {
+            friends_warning_text.setVisibility(View.VISIBLE);
+            if(bookmarkFrag != null)
+                getSupportFragmentManager().beginTransaction().remove(bookmarkFrag).commit();
+        }
     }
 }
--- RecipeFinder/app/src/main/java/com/example/gustav/recipefinder/fragments/ProfileTop.java
+++ RecipeFinder/app/src/main/java/com/example/gustav/recipefinder/fragments/ProfileTop.java
@@ -3,22 +3,48 @@ package com.example.gustav.recipefinder.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.gustav.recipefinder.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProfileTop extends Fragment {
+    private OnFragmentInteractionListener mListener;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     private String uID;
+
+    // Ifall den inloggade användaren har skickat en friendrequest
+    private boolean sent_friend_request = false;
+    // Ifall den inloggade användaren har fått en friendrequest
+    private boolean recieved_friend_request = false;
+    private boolean friends = false;
+
+    /*
+    0 = Inte vänner
+    1 = Pending friend request
+    2 = Vänner
+     */
+    private int friend_state = 0;
 
     public ProfileTop() {
         // Required empty public constructor
@@ -36,6 +62,26 @@ public class ProfileTop extends Fragment {
             uID = bundle.getString("uID");
         }
     }
+    /*
+    private void add_friend() { KLAR
+        DatabaseReference ref = mDatabase.child("friend_requests/"+uID);
+        Map<String, Object> updates = new HashMap<>();
+        updates.put(mAuth.getUid(), true);
+        ref.updateChildren(updates);
+    }
+
+    private void remove_friend() {
+    }
+
+    private void deny_friend_request() {
+
+    }
+
+    // TODO: Dubbelkolla så detta fungerar ifall det finns mer än 1 request för användaren
+    private void cancel_friend_request() { KLAR
+        mDatabase.child("friend_requests/"+uID+"/"+mAuth.getUid()).removeValue();
+    }
+    */
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,10 +89,18 @@ public class ProfileTop extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         readBundle(getArguments());
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
+        setup_description_listener();
+        if(!mAuth.getUid().equals(uID)) {
+            setup_friend_listener();
+        }
+    }
+
+    private void setup_description_listener() {
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference("users/"+uID+"");
 
@@ -68,18 +122,149 @@ public class ProfileTop extends Fragment {
                 System.out.println("The read failed: " + databaseError.getCode());
             }
         });
+    }
 
-        // Inflate the layout for this fragment
+    private void setup_friend_listener() {
+        final ImageButton friend_button = getView().findViewById(R.id.friend_button);
+        final TextView friend_status_text = getView().findViewById(R.id.friend_status_text);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference ref = database.getReference("friend_requests");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                sent_friend_request = false;
+                recieved_friend_request = false;
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Map<String, Object> sender = (Map<String, Object>) snapshot.getValue();
+                    String reciever = snapshot.getKey();
+                    // Ifall den inloggade användaren har skickat en friendrequest
+                    if(reciever.equals(uID)) {
+                        if(sender.containsKey(mAuth.getUid())) {
+                            sent_friend_request = true;
+                            mListener.friend_status_changed(false);
+                            friend_status_text.setText(R.string.friendPending);
+                            friend_button.setBackgroundResource(R.drawable.remove_friend);
+                            friend_button.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    mListener.cancel_friend_request(uID);
+                                }
+                            });
+                        }
+                    }
+                    // Ifall den inloggade användaren har fått en friendrequest
+                    if(reciever.equals(mAuth.getUid())) {
+                        if(sender.containsKey(uID)) {
+                            recieved_friend_request = true;
+                            mListener.friend_status_changed(false);
+                            friend_status_text.setText(R.string.friendRecieved);
+                            friend_button.setBackgroundResource(R.drawable.add_friend);
+                            friend_button.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    mListener.friend_status_changed(true);
+                                    mListener.accept_friend_request(uID);
+
+                                }
+                            });
+                        }
+                    }
+                }
+                if(!sent_friend_request && !recieved_friend_request && !friends) {
+                    mListener.friend_status_changed(false);
+                    friend_status_text.setText(R.string.notFriends);
+                    friend_button.setBackgroundResource(R.drawable.add_friend);
+                    friend_button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mListener.add_friend(uID);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        ref = database.getReference("user_friends");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                friends = false;
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String key = snapshot.getKey();
+                    Map<String, Object> values = (Map<String, Object>) snapshot.getValue();
+
+                    if(key.equals(uID)) {
+                        if(values.containsKey(mAuth.getUid())) {
+                            friends = true;
+                            mListener.friend_status_changed(true);
+                            friend_status_text.setText(R.string.friends);
+                            friend_button.setBackgroundResource(R.drawable.remove_friend);
+                            friend_button.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    mListener.friend_status_changed(false);
+                                    mListener.remove_friend(uID);
+                                }
+                            });
+                        }
+                    }
+                }
+
+                if(!sent_friend_request && !recieved_friend_request && !friends) {
+                    mListener.friend_status_changed(false);
+                    friend_status_text.setText(R.string.notFriends);
+                    friend_button.setBackgroundResource(R.drawable.add_friend);
+                    friend_button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mListener.add_friend(uID);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile_top, container, false);
     }
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void remove_friend(String friend_id);
+        void add_friend(String friend_id);
+        void cancel_friend_request(String friend_id);
+        void accept_friend_request(String friend_id);
+        void deny_friend_request(String friend_id);
+        void friend_status_changed(boolean friends);
     }
 }
